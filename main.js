49bcc7d1956ad70faceda8bc2f9b52246d4fdcd5
Andrew Ward <redrawdawn@gmail.com>
2022-02-18 07:56:07 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const add = (num1, num2) => {
+    const sum = num1 + num2
+    return sum
+}
+
+const subtract = (num1, num2) => {
+    const subtract = num1 - num2
+    return subtract
+}
+
+const divide = (num1, num2) => {
+    const divide = num1 / num2
+    return divide
+}
+
+const square = (num1, num2) => {
+    const square = num1 * num2
+    return square
+}
+
+result = add(20, 25)
+console.log(result)
+
+result = subtract(20, 15)
+console.log(result)
+
+result = divide(20, 4)
+console.log(result)
+
+result = square(3, 3)
+console.log(result)
